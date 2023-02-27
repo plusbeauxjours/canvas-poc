cf31e8fcbc57cb2ca15e728fad6f602d9a0032ed
plusbeauxjours <plusbeauxjours@gmail.com>
2023-02-27 20:50:20 +0700

    moveTo & lineTo

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -10,17 +10,15 @@ export const Canvas = (props) => {
     canvas.width = 800;
     canvas.height = 800;
 
-    // ctx.fillRect(0, 0, 400, 400);
-    ctx.rect(0, 0, 100, 100);
-    ctx.rect(100, 100, 100, 100);
-    ctx.rect(200, 200, 100, 100);
-    ctx.rect(300, 300, 100, 100);
+    ctx.moveTo(50, 50);
+    ctx.lineTo(150, 50);
+    ctx.lineTo(150, 150);
+    ctx.lineTo(50, 150);
+    ctx.lineTo(50, 50);
+    ctx.strokeStyle = 'cyan';
     ctx.fillStyle = 'tomato';
     ctx.fill();
-    ctx.beginPath();
-    ctx.fillStyle = 'cyan';
-    ctx.rect(400, 400, 100, 100);
-    ctx.fill();
+    ctx.stroke();
 
     // ... draw initial canvas here ...
   }, []);
