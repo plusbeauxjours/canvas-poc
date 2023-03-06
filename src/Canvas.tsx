ccff83721f24406c4d407371bb548668fd39469c
Minjae Lee <lmj.comcom@gmail.com>
2023-03-06 22:07:22 +0700

    line color

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,17 +1,24 @@
 import React, {useRef, useEffect, useState} from 'react';
+import styled from 'styled-components';
+import {colors} from './constants';
 
 export const Canvas = ({...props}) => {
   const canvasRef = useRef(null);
-  const [lineWidth, setLineWidth] = useState<string>('5')
-  const [lineColor, setLineColor] = useState<string>('#000')
+  const [lineWidth, setLineWidth] = useState<string>('5');
+  const [lineColor, setLineColor] = useState<string>('#000');
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
     ctx.lineWidth = lineWidth;
+  }, [lineWidth]);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
     ctx.strokeStyle = lineColor;
     ctx.fillStyle = lineColor;
-  }, [lineWidth, lineColor])
+  }, [lineColor]);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -19,8 +26,6 @@ export const Canvas = ({...props}) => {
     canvas.width = 800;
     canvas.height = 800;
 
-    ctx.strokeStyle = lineColor;
-    ctx.fillStyle = lineColor;
     ctx.lineWidth = lineWidth;
     let isPainting = false;
 
@@ -44,25 +49,45 @@ export const Canvas = ({...props}) => {
       ctx.lineWidth = event.target.value;
     }
 
-    canvas.addEventListener("mousemove", onMove);
-    canvas.addEventListener("mousedown", startPainting);
-    canvas.addEventListener("mouseup", cancelPainting);
-    canvas.addEventListener("mouseleave", cancelPainting);
+    canvas.addEventListener('mousemove', onMove);
+    canvas.addEventListener('mousedown', startPainting);
+    canvas.addEventListener('mouseup', cancelPainting);
+    canvas.addEventListener('mouseleave', cancelPainting);
   }, []);
 
   const onChangeLineWidth = (event) => {
-    setLineWidth(event.target.value)
-  }
+    setLineWidth(event.target.value);
+  };
 
-  const onChangeLineColor = (event) => {
-    setLineColor(event.target.value)
-  }
+  const onClickLineColor = (color) => {
+    setLineColor(color);
+  };
 
   return (
     <>
       <canvas ref={canvasRef} {...props} />
-      <input type="range" min="1" max="10" step='0.1' value={lineWidth} onChange={onChangeLineWidth}/>
-      <input type="color" value={lineColor} onChange={onChangeLineColor}/>
+      <input
+        type="range"
+        min="1"
+        max="10"
+        step="0.1"
+        value={lineWidth}
+        onChange={onChangeLineWidth}
+      />
+      <input type="color" value={lineColor} />
+      {colors.map((color, index) => (
+        <ColorBox
+          key={index}
+          style={{backgroundColor: color}}
+          onClick={() => onClickLineColor(color)}
+        />
+      ))}
     </>
-  )
+  );
 };
+
+const ColorBox = styled.div`
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+`;
