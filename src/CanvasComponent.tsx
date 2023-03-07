ef265abc223842604e727aed9de68ee16f73b0cf
Minjae Lee <lmj.comcom@gmail.com>
2023-03-07 10:26:16 +0700

    erase & fill

--- src/CanvasComponent.tsx
+++ src/CanvasComponent.tsx
@@ -8,6 +8,7 @@ export const CanvasComponent = ({...props}) => {
   const [lineColor, setLineColor] = useState<string>(colors[0]);
 
   let isPainting = false;
+  let isFilling = false;
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -28,8 +29,15 @@ export const CanvasComponent = ({...props}) => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
 
-    canvas.width = 800;
-    canvas.height = 800;
+    const modeBtn = document.getElementById('mode-btn');
+    const destroyBtn = document.getElementById('destroy-btn');
+    const eraserBtn = document.getElementById('eraser-btn');
+
+    const CANVAS_WIDTH = 800;
+    const CANVAS_HEIGHT = 800;
+
+    canvas.width = CANVAS_WIDTH;
+    canvas.height = CANVAS_HEIGHT;
 
     ctx.lineWidth = lineWidth;
 
@@ -52,10 +60,41 @@ export const CanvasComponent = ({...props}) => {
       ctx.beginPath();
     }
 
+    function onDestroyClick() {
+      ctx.fillStyle = 'white';
+      ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+
+    function onEraserClick() {
+      ctx.strokeStyle = 'white';
+    }
+
+    function onModeClick() {
+      console.log('isFilling', isFilling);
+      if (isFilling) {
+        isFilling = false;
+        modeBtn.innerText = 'Fill';
+      } else {
+        isFilling = true;
+        modeBtn.innerText = 'Draw';
+      }
+    }
+
+    function onCanvasClick() {
+      if (isFilling) {
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+      }
+    }
+
     canvas.addEventListener('mousemove', onMove);
     canvas.addEventListener('mousedown', startPainting);
     canvas.addEventListener('mouseup', cancelPainting);
     canvas.addEventListener('mouseleave', cancelPainting);
+    canvas.addEventListener('click', onCanvasClick);
+
+    modeBtn.addEventListener('click', onModeClick);
+    destroyBtn.addEventListener('click', onDestroyClick);
+    eraserBtn.addEventListener('click', onEraserClick);
   }, []);
 
   const onChangeLineWidth = (event) => {
@@ -85,6 +124,9 @@ export const CanvasComponent = ({...props}) => {
           onClick={() => onClickLineColor(color)}
         />
       ))}
+      <Btn id="mode-btn">Fill</Btn>
+      <Btn id="destroy-btn">Destroy</Btn>
+      <Btn id="eraser-btn">Erase</Btn>
     </>
   );
 };
@@ -98,3 +140,5 @@ const ColorBox = styled.div`
   height: 50px;
   cursor: pointer;
 `;
+
+const Btn = styled.button``;
